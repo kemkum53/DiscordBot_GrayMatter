57c0759502c2306e4c7f31b1d673d3c9405b1bba
kemkum53 <kondakci.k@gmail.com>
2021-06-21 13:54:07 +0300

    Updated

--- DiscordBot_GrayMatter/ConfigJson.cs
+++ DiscordBot_GrayMatter/ConfigJson.cs
@@ -12,6 +12,6 @@ namespace DiscordBot_GrayMatter
         [JsonProperty("token")]
         public string Token { get; private set; }
         [JsonProperty("prefix")]
-        public string Prefix { get; set; }
+        public string Prefix { get; private set; }
     }
 }
