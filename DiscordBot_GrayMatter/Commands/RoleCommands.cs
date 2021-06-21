7ae967b5541d7699d4fb24a405dca71f5571a764
kemkum53 <kondakci.k@gmail.com>
2021-06-21 13:40:58 +0300

    Added new commands

---
+++ DiscordBot_GrayMatter/Commands/RoleCommands.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot_GrayMatter.Commands
+{
+    class RoleCommands : BaseCommandModule
+    {
+
+    }
+}
